07cdab18fabeb7b6be73c06f36c9bbaed04fe1c6
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2018-11-11 11:43:09 +0100

    Initial commit

---
+++ src/test/java/me/AppTest.java
@@ -0,0 +1,12 @@
+package me;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
