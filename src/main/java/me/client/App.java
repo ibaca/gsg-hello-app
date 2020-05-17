071191706418afd2ce8c5dd1b62f6ea7609899eb
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2020-05-17 20:36:49 +0200

    Spice it up

--- src/main/java/me/client/App.java
+++ src/main/java/me/client/App.java
@@ -1,15 +1,20 @@
 package me.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.dom.client.Document;
-import com.google.gwt.dom.client.Text;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.intendia.rxgwt2.elemento.RxElemento;
+import elemental2.dom.HTMLElement;
+import org.jboss.gwt.elemento.core.Elements;
+
+import static elemental2.dom.DomGlobal.document;
+import static org.jboss.gwt.elemento.core.EventType.mousemove;
 
 public class App implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
-        Text textNode = Document.get().createTextNode("Hello World!");
-        RootPanel.getBodyElement().appendChild(textNode);
+        HTMLElement el = Elements.span().style("font-weight: bold;").get();
+        Elements.body().add("mouse position: ").add(el);
+        RxElemento.fromEvent(document, mousemove)
+                .subscribe(ev -> el.textContent = ev.clientX + ", " + ev.clientY);
     }
 }
